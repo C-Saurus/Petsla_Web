475596db3de979f68630d5e1cbc5580177a0140a
izanamiz <vanhtran112@gmail.com>
2022-11-17 20:02:53 +0700

    Update call api in Product Details

--- src/feature/Shop/service/productSlice.js
+++ src/feature/Shop/service/productSlice.js
@@ -1,41 +1,53 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import axios from 'axios';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 export const selectProductReducer = createSlice({
-    name: 'product',
-    initialState: {
-        status: 'idle',
-        product: {}
+  name: "product",
+  initialState: {
+    status: "idle",
+    product: {},
+  },
+  reducers: {
+    // action creators
+    selectProduct: (state, action) => {
+      console.log("state: ", state);
+      state.product = action.payload;
     },
-    reducers: {
-        // action creators
-        selectProduct: (state, action) => {
-            console.log('state: ', state)
-            state.product = action.payload;
-        },
-        removeProduct: (state) => {
-            state.product = {};
-        }
+    removeProduct: (state) => {
+      state.product = {};
     },
-    extraReducers: builder => {
-        builder.addCase(fetchProductDetail.pending, (state, action) => {
-            state.status = 'loading';
-        }).addCase(fetchProductDetail.fulfilled, (state, action) => {
-            state.product = action.payload;
-            state.status = 'idle';
-        })
-    }
-})
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProductDetail.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchProductDetail.fulfilled, (state, action) => {
+        state.product = action.payload;
+        state.status = "idle";
+      });
+  },
+});
 
-export const fetchProductDetail = createAsyncThunk('product/fetchProductDetail', async (productId) => {
-    const response = await axios
-        .get("http://petsla-api.herokuapp.com/products/")
-        .catch((err) => {
-            console.log("Err: ", err);
-        });
-    const selected = response.data.filter(ele => ele.id.toString() === productId)
-    return selected[0];
-})
+export const fetchProductDetail = createAsyncThunk(
+  "product/fetchProductDetail",
+  async (productId) => {
+    const tmp = await axios
+      .get(`http://petsla-api.herokuapp.com/product/${productId}`)
+      .catch((err) => {
+        console.log("Err: ", err);
+      });
+    // console.log("product:", tmp.data);
+    return tmp.data;
 
-
-
+    // const response = await axios
+    //   .get("http://petsla-api.herokuapp.com/products/")
+    //   .catch((err) => {
+    //     console.log("Err: ", err);
+    //   });
+    // const selected = response.data.filter(
+    //   (ele) => ele.id.toString() === productId
+    // );
+    // return selected[0];
+  }
+);
